ce170dd7865878f808f2f6bbadb70bdaf2acfb31
Rajabu Mrisho <rajabumrisho69@gmail.com>
2024-06-22 11:11:14 +0300

    ---

--- assets/static/chunks/pages/_error-1be831200e60c5c0.js
+++ assets/static/chunks/pages/_error-1be831200e60c5c0.js
@@ -1,5 +1,8 @@
 (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
-    [820], { 1981: function(n, _, u) {
-            (window._assets_P = window._assets_P || []).push(["/_error", function() { return u(3387) }]) } },
+    [820], {
+        1981: function(n, _, u) {
+            (window._assets_P = window._assets_P || []).push(["/_error", function() { return u(3387) }])
+        }
+    },
     function(n) { n.O(0, [888, 774, 179], function() { return n(n.s = 1981) }), _N_E = n.O() }
 ]);
